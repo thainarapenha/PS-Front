4967a91e84aa09d515c69a2b5032474df6eb3dc1
Thainara <thainarapenha@alu.ufc.br>
2022-09-03 16:08:47 -0300

    estilizando a página

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,20 @@ import { ShareFriends } from './components/ShareFriends/ShareFriends'
 import { FormNewsLatters } from './components/FormNewsLatters/FormNewsLatters'
 import './App.css'
 
-function App() {
+const App = () => {
   // const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
+    <div class="headerMain">
       <Header/>
+    </div>
       <FormNewsLatters/>
       <ShareFriends/>
-      <Footer/>
+      <div className="footerMain">
+        <Footer/>
+      </div>
     </div>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,24 +2,27 @@ import './Header.css'
 import Button from '@mui/material/Button';
 
 export function Header(){
-    return(
-        <div className="cabecalho">
-            <header>
-                <div className='texto'>
-                    <h2>Uma seleção de produtos</h2>
-                    <strong><h1>Especial para você</h1></strong>
-                    <p>Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</p>
-                </div>
 
-                <nav className='navbar'>
-                    <ul>
-                        <li><Button variant="contained">Conheça a Linx</Button></li>
-                        <li><Button variant="contained">Ajude o algorítimo</Button></li>
-                        <li><Button variant="contained">Seus produtos</Button></li>
-                        <li><Button variant="contained">Compartilhe</Button></li>
-                    </ul>
-                </nav>
-            </header>
+    return(
+        <div className='containerHeader' id="tsparticles">
+            <div className="cabecalho">
+                <header>
+                    <div className='texto'>
+                        <h2>Uma seleção de produtos</h2>
+                        <strong><h1>Especial para você</h1></strong>
+                        <p>Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</p>
+                    </div>
+
+                    <nav className='navbar'>
+                        <ul>
+                            <li><Button variant="contained">Conheça a Linx</Button></li>
+                            <li><Button variant="contained">Ajude o algorítimo</Button></li>
+                            <li><Button variant="contained">Seus produtos</Button></li>
+                            <li><Button variant="contained">Compartilhe</Button></li>
+                        </ul>
+                    </nav>
+                </header>
+            </div>
         </div>
     );
 }
